39b33a7e52bcf42eedaf137a6c0934880f0c6097
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2017-03-02 11:06:01 -0600

    Update cpacSuv_sge.sh
    Better memory administration, files were corrected.
    FAB Feb2017

--- cpacSuv_sge.sh
+++ cpacSuv_sge.sh
@@ -1,30 +1,31 @@
 #! /bin/bash
-## SGE batch file - bgsp
+## SGE batch file - cpacFAB
 #$ -S /bin/bash
-## bgsp is the jobname and can be changed
-#$ -N bgsp_fab
+## cpacFAB is the jobname and can be changed
+#$ -N cpac_fab
 ## execute the job using the mpi_smp parallel enviroment and 8 cores per job
 #$ -pe mpi_smp 8
 ## create an array of 1112 jobs
-#$ -t 1-1112
+#$ -t 1-48
 #$ -V
+#$ -l mem_free=2G
 ## change the following working directory to a persistent directory that is
 ## available on all nodes, this is were messages printed by the app (stdout
 ## and stderr) will be stored
-#$ -wd /mnt/MD1200A/fbarrios/fbarrios
+#$ -wd /mnt/MD1200A/fbarrios/cpac_outputs
 
 ## sudo chmod 777 /mnt
-mkdir -p /mnt/MD1200A/fbarrios/fbarrios/log/reports
+mkdir -p /mnt/MD1200A/fbarrios/cpac_outputs/log/reports
 
 sge_ndx=$(( SGE_TASK_ID - 1 ))
 
 # random sleep so that jobs dont start at _exactly_ the same time
 sleep $(( $SGE_TASK_ID % 10 ))
 
-singularity run -B /mnt:/mnt \
+singularity run -B /mnt:/mnt -B /tmp:/tmp \
   /mnt/MD1200A/fbarrios/fbarrios/singularity_images/cpac_v1.0.0 \
-  --n_cpus 8 --mem 12 \
-  --data_config_file /mnt/workspace/cluster_files/bgsp_data_config.yml \
-  /mnt/MD1200A/fbarrios/orig_bids/ \
-  /mnt/MD1200A/fbarrios/cpac_out/ \
+  --n_cpus 8 --mem 16 \
+  --data_config_file /mnt/MD1200A/fbarrios/rsConRDC/cpac_data_config_20161104231240.yml \
+  /mnt/MD1200A/fbarrios/rsConRDC/ \
+  /mnt/MD1200A/fbarrios/rsConRDC/outputs_config_20170228/ \
   participant --participant_ndx ${sge_ndx}
