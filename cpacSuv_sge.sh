9958fcb8115b7bb96dc6e735f7b35d36921042bb
vicmat005 <vicmat005@gmail.com>
2017-03-03 12:32:08 -0600

    Cambios para ADA
    Copiado de FAB script para mandar a cola usando maquina virtual cpac

--- cpacSuv_sge.sh
+++ cpacSuv_sge.sh
@@ -12,11 +12,11 @@
 ## change the following working directory to a persistent directory that is
 ## available on all nodes, this is were messages printed by the app (stdout
 ## and stderr) will be stored
-#$ -wd /mnt/MD1200A/fbarrios/cpac_cluster_files/
+#$ -wd /mnt/MD1200A/fbarrios/volalde/cpac_cluster_files/
 
 module add singularity/2.2
 ## sudo chmod 777 /mnt
-mkdir -p /mnt/MD1200A/fbarrios/cpac_cluster_files/log/reports
+mkdir -p /mnt/MD1200A/fbarrios/volalde/cpac_cluster_files/log/reports
 
 sge_ndx=$(( SGE_TASK_ID - 1 ))
 
@@ -26,8 +26,8 @@ sleep $(( $SGE_TASK_ID % 10 ))
 singularity run -B /mnt:/mnt  \
   /mnt/MD1200A/fbarrios/fbarrios/singularity_images/cpac_v1.0.0 \
   --n_cpus 8 --mem 16 \
-  --pipeline_file /mnt/MD1200A/fbarrios/cpac_rsConRDC/pipeline_config_FAB.yml \
-  --data_config_file /mnt/MD1200A/fbarrios/cpac_rsConRDC/cpac_data_config_20161104231240.yml \
-  /mnt/MD1200A/fbarrios/rsConRDC/ \
-  /mnt/MD1200A/fbarrios/cpac_rsConRDC/outputs_FAB/ \
+  --pipeline_file /mnt/MD1200A/fbarrios/volalde/cpac_BIDS_rdc/pipeline_config_VOL.yml \
+  --data_config_file /mnt/MD1200A/fbarrios/volalde/cpac_BIDS_rdc/cpac_data_config_VOL.yml \
+  /mnt/MD1200A/fbarrios/BIDS/BIDS_rdc/ \
+  /mnt/MD1200A/fbarrios/volalde/cpac_BIDS_rdc/outputs_RDC/ \
   participant --participant_ndx ${sge_ndx} 
